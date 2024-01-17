b10f20c836519b45cffb023c334552d4b5490df7
Diego Santana <3c.santana@gmail.com>
2024-01-17 17:53:33 -0300

    Configurações iniciais do projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuizComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
